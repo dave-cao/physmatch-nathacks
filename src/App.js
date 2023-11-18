ff0665aa63d2dc0796bd1fe48895bd56117e09a3
jaiohri <ohrijai2003@gmail.com>
2023-11-18 15:29:48 -0700

    map works

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
   let routes;
 
   // routes for when doctor is logged in (doctor view)
-  if (true) {
+  if (false) {
     routes = (
       <Routes>
         <Route exact path="/clients" element={<Clients />} />
